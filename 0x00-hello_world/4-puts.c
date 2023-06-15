cba02a361a5017567b27fe7e4cabbaac6052f163
Katlego Nkutha <katlegognkutha>
2023-06-15 04:03:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
 /**
-*main – Entry point 
-*
-*Return:Always 0(Success) 
-*/
-Int main(void)
+ * main - A program that prints a line with puts function
+ * Return: 0(Success)
+ */
+int main(void)
 {
-                Puts(“\”Programming is like building a multilingual puzzle”);
-                Return (0);
-} 
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
