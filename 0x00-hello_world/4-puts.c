5560b7f161fe6256f682480e906094ad759a493f
Katlego Nkutha <katlegognkutha>
2023-06-15 03:34:31 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+*main – Entry point 
+*
+*Return:Always 0(Success) 
+*/
+Int main(void)
+{
+                Puts(“\”Programming is like building a multilingual puzzle”);
+                Return (0);
+} 
+
