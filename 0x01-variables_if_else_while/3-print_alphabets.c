c3ae00188836216bbf858577b72570f1225535a9
Katlego Nkutha <katlegognkutha>
2023-06-19 03:54:07 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Prints the alphaBET.
+ *
+ * Return: Lawyas 0 (succuess)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+for (i = 0; i < 52; i++)
+{ 
+	putchar(aalp[i]);
+}
+putchar('\n');
+return (0);
+}
